f14164f9fc0c938aac2824f5f3933bab1a0f654c
Siddhi Kansal <siddhi.kansal.cse.2025@miet.ac.in>
2026-09-25 19:22:00 +0530

    Removed note1.c

--- binarysearch.c
+++ binarysearch.c
@@ -21,18 +21,3 @@ int binarySearch(int arr[], int n, int target)
     return -1;
 }
 
-int main()
-{
-    int arr[] = {1, 3, 5, 7, 9, 11, 13};
-    int n = 7;
-    int target = 9;
-
-    int result = binarySearch(arr, n, target);
-
-    if (result != -1)
-        printf("Element found at index: %d\n", result);
-    else
-        printf("Element not found\n");
-
-    return 0;
-}
